00b1a640be7779d4de1cd3fb3849f76f5a3ce7aa
Savostyanov Sergey <395105@mail.ru>
2022-12-13 17:34:21 +0400

    - Added DTO "address"

---
+++ edo-common/src/main/java/dto/AddressDTO.java
@@ -0,0 +1,44 @@
+package dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.*;
+
+@Getter
+@Setter
+@ApiModel(value = "Объект для передачи данных")
+public class AddressDTO {
+
+    @ApiModelProperty(value = "id")
+    private Long id;
+
+    @ApiModelProperty(value = "полный адрес")
+    private String fullAddress;
+
+    @ApiModelProperty(value = "Название улицы")
+    private String street;
+
+    @ApiModelProperty(value = "Номер дома")
+    private String house;
+
+    @ApiModelProperty(value = "Почтовый индекс")
+    private Integer index;
+
+    @ApiModelProperty(value = "Корпус")
+    private String housing;
+
+    @ApiModelProperty(value = "Строение")
+    private String building;
+
+    @ApiModelProperty(value = "Город")
+    private String city;
+
+    @ApiModelProperty(value = "Регион")
+    private String region;
+
+    @ApiModelProperty(value = "Страна")
+    private String country;
+
+    @ApiModelProperty(value = "Квартира")
+    private String flat;
+}
