41ea54dcbb76bcc175bf8266c16dd392c3adb107
Rodion Ivanov <rodionivanov93@gmail.com>
2022-12-13 23:24:20 +0300

    Revert "Add annotation Entity"
    This reverts commit 118bbff72410369436de99e537aec60ad90b032f.

--- edo-repository/src/main/java/com/education/entity/Employee.java
+++ edo-repository/src/main/java/com/education/entity/Employee.java
@@ -1,8 +1,7 @@
 package com.education.entity;
 
-import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-@Entity
+
 @Table(name = "employee")
 public class Employee extends BaseEntity {
 
