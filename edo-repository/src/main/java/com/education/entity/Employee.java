118bbff72410369436de99e537aec60ad90b032f
Rodion Ivanov <rodionivanov93@gmail.com>
2022-12-13 23:07:38 +0300

    Add annotation Entity

--- edo-repository/src/main/java/com/education/entity/Employee.java
+++ edo-repository/src/main/java/com/education/entity/Employee.java
@@ -1,7 +1,8 @@
 package com.education.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-
+@Entity
 @Table(name = "employee")
 public class Employee extends BaseEntity {
 
