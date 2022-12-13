ec11a9d965261e8282f6ad4fcf963cbf13272d85
Savostyanov Sergey <395105@mail.ru>
2022-12-13 17:33:37 +0400

    - Added an entity "address"

---
+++ edo-repository/src/main/java/com/education/entity/Address.java
@@ -0,0 +1,80 @@
+package com.education.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+/**
+ * Сущность описывающая адрес проживания пользователя отправившего обращение
+ */
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@Builder
+@Table(name = "address")
+public class Address extends BaseEntity{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    /**
+     * Полный адрес
+     */
+    @Column(name = "full_address")
+    private String fullAddress;
+
+    /**
+     * Название улицы
+     */
+    @Column(name = "street")
+    private String street;
+
+    /**
+     * Дом
+     */
+    @Column(name = "house")
+    private String house;
+
+    /**
+     * Почтовый индекс
+     */
+    @Column(name = "index")
+    private Integer index;
+
+    /**
+     * Корпус
+     */
+    @Column(name = "housing")
+    private String housing;
+
+    /**
+     * Строение
+     */
+    @Column(name = "building")
+    private String building;
+
+    /**
+     * Город
+     */
+    @Column(name = "city")
+    private String city;
+
+    /**
+     * Регион
+     */
+    @Column(name = "region")
+    private String region;
+
+    /**
+     * Страна
+     */
+    @Column(name = "country")
+    private String country;
+
+    /**
+     * Квартира
+     */
+    @Column(name = "flat")
+    private String flat;
+}
