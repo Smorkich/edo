981d37bbb64fc5f1df2de3d56c8cf629f1299192
Savostyanov Sergey <395105@mail.ru>
2022-12-13 17:30:36 +0400

    -Added migration for the entity address

---
+++ edo-repository/src/main/resources/db/migration/v.1/V1_13122022104013__create_address_table.sql
@@ -0,0 +1,44 @@
+create table if not exists address
+(
+    id           bigserial not null primary key,
+    full_address varchar(60),
+    street       varchar(30),
+    house        varchar(10),
+    index        int,
+    housing      varchar(10),
+    building     varchar(10),
+    city         varchar(30),
+    region       varchar(30),
+    country      varchar(30),
+    flat         varchar(30)
+);
+
+comment on column address.full_address
+    is 'Полный адрес';
+
+comment on column address.street
+    is 'Название улицы';
+
+comment on column address.house
+    is 'Номер дома';
+
+comment on column address.index
+    is 'Почтовый индекс';
+
+comment on column address.housing
+    is 'Корпус';
+
+comment on column address.building
+    is 'Строение';
+
+comment on column address.city
+    is 'Город';
+
+comment on column address.region
+    is 'Регион';
+
+comment on column address.country
+    is 'Страна';
+
+comment on column address.flat
+    is 'Квартира'
