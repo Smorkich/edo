3ab77cf05fb0fd5e91206bfa893cb20f9886d8bb
Akhmed Guseynov <akhmeddzhan@mail.ru>
2022-12-14 14:03:16 +0000

    Upload New File

---
+++ edo-repository/src/main/resources/db/migration/v.1/V1_0112312432123__create_nomenclature_table.sql
@@ -0,0 +1,17 @@
+
+create table if not exists Nomenclature (
+    id int primary key,
+    creation_date DATE,
+    archived_date DATE,
+    template varchar(300),
+    current_value int,
+    "index" int
+);
+
+comment on table Nomenclature is 'Таблица номенкталура';
+comment on column Nomenclature.id is 'идентификатор';
+comment on column Nomenclature.creation_date is 'дата создания';
+comment on column Nomenclature.archived_date is 'дата архивации';
+comment on column Nomenclature.template is 'шаблон записи';
+comment on column Nomenclature.current_value is 'текущее значение';
+comment on column Nomenclature."index" is 'индекс записи';
