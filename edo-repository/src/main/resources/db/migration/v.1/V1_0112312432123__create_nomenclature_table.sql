96d45a4de1a53261a1aa2508660e8d0305f68ffa
Akhmed Guseynov <akhmeddzhan@mail.ru>
2022-12-15 09:18:40 +0000

    Update V1_0112312432123__create_nomenclature_table.sql

--- edo-repository/src/main/resources/db/migration/v.1/V1_0112312432123__create_nomenclature_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_0112312432123__create_nomenclature_table.sql
@@ -1,16 +1,18 @@
 create table if not exists nomenclature (
     id serial primary key not null,
-    creation_date DATE,
-    archived_date DATE,
+    creation_date timestamp with time zone NOT NULL
+   DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
+    archived_date timestamp with time zone NOT NULL
+   DEFAULT (current_timestamp AT TIME ZONE 'UTC'),
     template varchar(300),
     current_value int,
     "index" int
 );
 
-comment on table Nomenclature is 'Таблица номенкталура';
-comment on column Nomenclature.id is 'идентификатор';
-comment on column Nomenclature.creation_date is 'дата создания';
-comment on column Nomenclature.archived_date is 'дата архивации';
-comment on column Nomenclature.template is 'шаблон записи';
-comment on column Nomenclature.current_value is 'текущее значение';
-comment on column Nomenclature."index" is 'индекс записи';
+comment on table nomenclature is 'Таблица номенкталура';
+comment on column nomenclature.id is 'идентификатор';
+comment on column nomenclature.creation_date is 'дата создания';
+comment on column nomenclature.archived_date is 'дата архивации';
+comment on column nomenclature.template is 'шаблон записи';
+comment on column nomenclature.current_value is 'текущее значение';
+comment on column nomenclature."index" is 'индекс записи';
