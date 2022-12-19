9de6889f9973fded9b39a4b49fd9caa2a73fe3f2
Rodion Ivanov <rodionivanov93@gmail.com>
2022-12-19 21:18:32 +0300

    add constraint in fields

--- edo-repository/src/main/resources/db/migration/v.1/V1_13122022160350__create_appeal_signer_table.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V1_13122022160350__create_appeal_signer_table.sql
@@ -1,5 +1,5 @@
 create table if not exists appeal_signer
 (
-    appeal_id bigint primary key,
-    employee_id bigint
+    appeal_id bigint primary key not null unique,
+    employee_id bigint not null unique
 );
