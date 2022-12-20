c3d55fb884c288df77085e84591b6043c5cea253
Savostyanov Sergey <395105@mail.ru>
2022-12-20 17:26:25 +0400

    - add logger

--- edo-repository/src/main/java/com/education/controller/AuthorRestController.java
+++ edo-repository/src/main/java/com/education/controller/AuthorRestController.java
@@ -4,6 +4,7 @@ import com.education.service.address.AuthorService;
 import com.education.util.AuthorUtil;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import model.dto.AuthorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -11,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
+import java.util.logging.Logger;
 
 /**
  * Рест-контроллер для Author
@@ -18,13 +20,16 @@ import java.util.Collection;
 
 @RestController
 @AllArgsConstructor
+@Log4j2
 @RequestMapping("/api/repository/author")
 public class AuthorRestController {
+
     private final AuthorService authorService;
 
     @ApiOperation(value = "Gets all authors", notes = "Author must exist")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Collection<AuthorDto>> getAllAuthors() {
+        log.info("Get: /api/repository/author");
         return new ResponseEntity<>(AuthorUtil.ListAuthorDtos(authorService.findAll()),
                 HttpStatus.OK);
     }
@@ -32,20 +37,25 @@ public class AuthorRestController {
     @ApiOperation(value = "Add author", notes = "Author not must exist")
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthorDto> addAuthorAction(@RequestBody AuthorDto authorDto) {
+        log.info("POST: /api/repository/author");
         authorService.save(AuthorUtil.toAuthor(authorDto));
+        log.info("POST request successful");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Delete author", notes = "Author must exist")
     @DeleteMapping("/{id}")
     public ResponseEntity<AuthorDto> deleteAuthor (@PathVariable Long id){
+        log.info("DELETE: /api/repository/author/"+id);
         authorService.delete(id);
+        log.info("DELETE request successful");
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @ApiOperation(value = "Gets authors by ids", notes = "Author must exist")
+    @ApiOperation(value = "Gets authors by id", notes = "Author must exist")
     @GetMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthorDto> getAuthorById(@PathVariable Long id) {
+        log.info("Get: /api/repository/author/"+id);
         return new ResponseEntity<>(AuthorUtil.toDto(authorService.findById(id)), HttpStatus.OK);
     }
 }
--- edo-service/src/main/java/com/education/controller/AuthorRestController.java
+++ edo-service/src/main/java/com/education/controller/AuthorRestController.java
@@ -3,6 +3,7 @@ package com.education.controller;
 import com.education.service.AuthorService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import model.dto.AuthorDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -10,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 /**
  * Рест-контроллер для Author
@@ -17,6 +19,7 @@ import java.util.List;
 
 @RestController
 @AllArgsConstructor
+@Log4j2
 @RequestMapping("/api/service/author")
 public class AuthorRestController {
     private final AuthorService authorService;
@@ -24,26 +27,32 @@ public class AuthorRestController {
     @ApiOperation(value = "Gets all authors", notes = "Author must exist")
     @GetMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<List<AuthorDto>> getAllAuthors() {
+        log.info("GET: /api/service/author");
         return new ResponseEntity<>(authorService.findAll(), HttpStatus.OK);
     }
 
     @ApiOperation(value = "Add author", notes = "Author not must exist")
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthorDto> addAuthorAction(@RequestBody AuthorDto authorDto) {
+        log.info("POST: /api/service/author");
         authorService.save(authorDto);
+        log.info("POST request successful");
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
     @ApiOperation(value = "Delete author", notes = "Author must exist")
     @DeleteMapping("/{id}")
     public ResponseEntity<AuthorDto> deleteAuthor(@PathVariable Long id) {
+        log.info("DELETE: /api/service/author/"+id);
         authorService.delete(id);
+        log.info("DELETE request successful");
         return new ResponseEntity<>(HttpStatus.ACCEPTED);
     }
 
-    @ApiOperation(value = "Gets authors by ids", notes = "Author must exist")
+    @ApiOperation(value = "Gets authors by id", notes = "Author must exist")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AuthorDto> getAuthorById(@PathVariable Long id) {
+        log.info("Get: /api/service/author/"+id);
         return new ResponseEntity<>(authorService.findById(id), HttpStatus.OK);
     }
 }
