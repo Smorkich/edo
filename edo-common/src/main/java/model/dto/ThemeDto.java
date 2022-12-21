32930a2dbb82df2286144b41a2995ccba1514436
AlexeySpiridonov <prograbota@mail.ru>
2022-12-21 13:05:09 +0000

    Upload New File

---
+++ edo-common/src/main/java/model/dto/ThemeDto.java
@@ -0,0 +1,32 @@
+package model.dto;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import java.time.ZonedDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@ApiModel(value = "Тема обращения")
+public class ThemeDto {
+
+    @ApiModelProperty(value = "номер id")
+    private Long id;
+
+    @ApiModelProperty(value = "название темы")
+    private String name;
+
+    @ApiModelProperty(value = "дата архивации")
+    private ZonedDateTime archivedDate;
+
+    @ApiModelProperty(value = "дата создания")
+    private ZonedDateTime creationDate;
+
+    @ApiModelProperty(value = "идентификатор темы")
+    private String code;
+}
