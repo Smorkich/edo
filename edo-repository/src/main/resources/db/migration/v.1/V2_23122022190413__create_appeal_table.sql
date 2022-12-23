f76fbcb5abd521cc70cd6669b681e635f4731591
Roman Bekker <bekker.roman@list.ru>
2022-12-23 19:45:03 +0300

    fix migration

---
+++ edo-repository/src/main/resources/db/migration/v.1/V2_23122022190413__create_appeal_table.sql
@@ -0,0 +1,27 @@
+alter table if exists appeal
+    add appeals_status         varchar(255),
+    add appeals_receipt_method varchar(255),
+    add appeal_authors         varchar(255),
+    add appeal_filepool        varchar(255),
+    add appeal_question        varchar(255),
+    add nomenclature_id        bigint references edo.nomenclature (id),
+    add resolution_id          bigint references edo.resolution (id);
+    --thema_id               bigint references edo.thema (id)
+
+
+comment on column appeal.appeals_status
+    is 'Статус обращения';
+comment on column appeal.appeals_receipt_method
+    is 'Способ получения обращения';
+comment on column appeal.appeal_authors
+    is 'Автор, соавторы обращения';
+comment on column appeal.appeal_filepool
+    is 'Несколько файлов';
+comment on column appeal.appeal_question
+    is 'Несколько вопросов';
+comment on column appeal.nomenclature_id
+    is 'Номенклатура';
+comment on column appeal.resolution_id
+    is 'Разрешение';
+--comment on column appeal.thema_id
+--    is 'Тема обращения';
