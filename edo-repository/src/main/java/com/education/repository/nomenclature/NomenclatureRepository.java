f6e0c27fc239dc70c910daa853614b837dfe0aec
Akhmed Guseynov <akhmeddzhan@mail.ru>
2022-12-24 13:43:58 +0000

    Update NomenclatureRepository.java

--- edo-repository/src/main/java/com/education/repository/nomenclature/NomenclatureRepository.java
+++ edo-repository/src/main/java/com/education/repository/nomenclature/NomenclatureRepository.java
@@ -12,16 +12,20 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 
+ /** JPA DATA Repository for edo-repository service */
 @Repository
 public interface NomenclatureRepository extends JpaRepository<Nomenclature,Long> {
 
+    /** Query request for set a date to archived_date field */
     @Modifying
     @Query(value = "update Nomenclature nomenclature set  nomenclature.archivedDate =:archDate where nomenclature.id =:id")
     void moveToArchive( @Param("archDate") ZonedDateTime date, @Param("id") Long id);
 
+    /** Query request for searching not archived entity by id */
     @Query("select u from Nomenclature u where ((u.archivedDate is null) and u.id =:id)")
     Optional<Nomenclature> findByIdNotArchived(@Param("id") Long id);
 
+    /** Query request for searching not archived entities by ids */
     @Query("select u from Nomenclature u where (u.archivedDate is null and u.id in :idList)")
     List<Nomenclature> findAllByIdNotArchived(@Param("idList") Collection<Long> idList);
 
