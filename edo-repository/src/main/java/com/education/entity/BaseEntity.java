8021644d78158d4e99787627ca0d303ed12eccb2
Akhmed Guseynov <akhmeddzhan@mail.ru>
2022-12-25 16:58:33 +0000

    Update BaseEntity.java

--- edo-repository/src/main/java/com/education/entity/BaseEntity.java
+++ edo-repository/src/main/java/com/education/entity/BaseEntity.java
@@ -16,7 +16,7 @@ import java.util.Objects;
 @MappedSuperclass
 public class BaseEntity {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Override
