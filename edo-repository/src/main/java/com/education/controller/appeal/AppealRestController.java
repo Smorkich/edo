f681432454febf168efa7a6a23956b39b9695683
Roman Bekker <bekker.roman@list.ru>
2023-01-03 20:33:01 +0300

    added logs

--- edo-repository/src/main/java/com/education/controller/appeal/AppealRestController.java
+++ edo-repository/src/main/java/com/education/controller/appeal/AppealRestController.java
@@ -2,7 +2,9 @@ package com.education.controller.appeal;
 
 import com.education.entity.Appeal;
 import com.education.service.appeal.AppealService;
+import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import model.dto.AppealDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -14,6 +16,7 @@ import java.util.Collection;
 
 import static com.education.mapper.AppealMapper.APPEAL_MAPPER;
 
+@Log4j2
 @RestController
 @AllArgsConstructor
 @RequestMapping("/api/repository/appeal")
@@ -21,43 +24,72 @@ public class AppealRestController {
 
     private AppealService appealService;
 
+    @ApiOperation(value = "В строке таблицы Appeal заполняет поле archivedDate",
+            notes = "Строка в Appeal должна существовать")
     @PatchMapping(value = "/move/{id}")
     public ResponseEntity<AppealDto> moveToArchive(@PathVariable Long id) {
+        log.info("Adding archived date {} in Appeal with id: {}", ZonedDateTime.now(), id);
         appealService.moveToArchive(id, ZonedDateTime.now());
+        log.info("Moving appeal with id: {} to archive is success!", id);
         return new ResponseEntity<>(HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Находит все строки таблицы Appeal с полем acrhivedDate = null",
+            notes = "Строка в Appeal должна существовать")
     @GetMapping(value = "/findAllNotArchived", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Collection<AppealDto>> findAllNotArchived() {
+        log.info("Getting from database all appeals with field acrhivedDate = null");
         var appealDtoCollection = APPEAL_MAPPER.toDto(appealService.findAllNotArchived());
+        log.info("Appeals: {}", appealDtoCollection);
         return new ResponseEntity<>(appealDtoCollection, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Находит строку таблицы Appeal c полем acrhivedDate = null, по заданному id",
+            notes = "Строка в Appeal должна существовать")
     @GetMapping(value = "/findByIdNotArchived/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AppealDto> findByIdNotArchived(@PathVariable Long id) {
-        return new ResponseEntity<>(APPEAL_MAPPER.toDto(appealService.findByIdNotArchived(id)), HttpStatus.OK);
+        log.info("Getting from database appeal with field acrhivedDate = null, with id: {}", id);
+        var appeal = appealService.findByIdNotArchived(id);
+        log.info("Appeal: {}", appeal);
+        return new ResponseEntity<>(APPEAL_MAPPER.toDto(appeal), HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Находит все строки таблицы Appeal",
+            notes = "Строка в Appeal должна существовать")
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Collection<AppealDto>> getAllAppeal() {
+        log.info("Getting from database all appeals");
         var appealDtoCollection = APPEAL_MAPPER.toDto(appealService.findAll());
+        log.info("Appeals: {}", appealDtoCollection);
         return new ResponseEntity<>(appealDtoCollection, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Удаляет строку таблицы Appeal по id",
+            notes = "Строка в Appeal должна существовать")
     @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Long> deleteAppeal(@PathVariable(value = "id") Long id) {
+        log.info("Deleting from database appeal with id: {}", id);
         appealService.delete(id);
+        log.info("Deleting from database appeal with id: {}, success!", id);
         return new ResponseEntity<>(id, HttpStatus.OK);
     }
 
+    @ApiOperation(value = "Добавляет новую строку таблицы Appeal")
     @PostMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AppealDto> saveAppeal(@RequestBody AppealDto appealDto) {
+        log.info("Creating appeal");
         Appeal save = appealService.save(APPEAL_MAPPER.toEntity(appealDto));
+        log.info("Creating appeal {}, success!", save);
         return new ResponseEntity<>(APPEAL_MAPPER.toDto(save), HttpStatus.CREATED);
     }
 
+    @ApiOperation(value = "Находит строку таблицы Appeal по id",
+            notes = "Строка в Appeal должна существовать")
     @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AppealDto> getAppealById(@PathVariable Long id) {
-        return new ResponseEntity<>(APPEAL_MAPPER.toDto(appealService.findById(id)), HttpStatus.OK);
+        log.info("Getting from database appeal with id: {}", id);
+        var appeal = appealService.findById(id);
+        log.info("Appeal: {}", appeal);
+        return new ResponseEntity<>(APPEAL_MAPPER.toDto(appeal), HttpStatus.OK);
     }
 }
