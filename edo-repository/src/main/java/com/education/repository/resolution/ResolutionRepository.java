32473b958017f0bb5b7866219831b719c508ef2c
Andrey Schapkov <a.schapkov@mail.ru>
2023-01-06 21:15:06 +0300

    change in parameter transmission

--- edo-repository/src/main/java/com/education/repository/resolution/ResolutionRepository.java
+++ edo-repository/src/main/java/com/education/repository/resolution/ResolutionRepository.java
@@ -4,9 +4,9 @@ import com.education.entity.Resolution;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.ZonedDateTime;
 import java.util.Collection;
 
 /**
@@ -17,20 +17,20 @@ public interface ResolutionRepository extends JpaRepository<Resolution, Long> {
     /**
      * Выборка резолюции по id и при этом она не архивирована
      */
-    @Query("SELECT res from Resolution res where res.id = ?1 and res.archivedDate is null")
-    public Resolution findByIdAndArchivedDateIsNull(Long id);
+    @Query("SELECT res from Resolution res where res.id =:id and res.archivedDate is null")
+    public Resolution findByIdAndArchivedDateIsNull(@Param("id") Long id);
 
     /**
      * Выборка всех резолюций не архивированных резолюций
      */
-    @Query("SELECT res from Resolution res where res.id = ?1 and res.archivedDate is null  ")
-    public Collection<Resolution> findAllByArchivedDateIsNull(Collection<Long> id);
+    @Query("SELECT res from Resolution res where res.id =:id and res.archivedDate is null")
+    public Collection<Resolution> findAllByArchivedDateIsNull(@Param("id") Collection<Long> id);
 
     /**
      * Перемещение резолюции в архив
      */
     @Modifying
-    @Query("UPDATE Resolution res set res.archivedDate = current_date() where res.id = ?1")
-    public void movesToArchive(Long id);
+    @Query("UPDATE Resolution res set res.archivedDate = current_date() where res.id =:id")
+    public void movesToArchive(@Param("id") Long id);
 
 }
