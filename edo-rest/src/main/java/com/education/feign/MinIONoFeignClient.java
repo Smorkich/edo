56d0ac7f585bb5d5968f2c95d4dc967a9cf7181b
Akhmed Guseynov <akhmeddzhan@mail.ru>
2023-01-08 13:41:49 +0000

    Update 4 files
    - /edo-rest/src/main/java/com/education/feign/MinIOFeignClient.java
    - /edo-scheduler/src/main/java/com/education/feign/FeignClient.java
    - /edo-rest/src/main/java/com/education/feign/MinIONoFeignClient.java
    - /edo-scheduler/src/main/java/com/education/feign/NoFeignClient.java

--- edo-rest/src/main/java/com/education/feign/MinIONoFeignClient.java
+++ edo-rest/src/main/java/com/education/feign/MinIONoFeignClient.java
@@ -3,9 +3,10 @@ import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
+
 /** Realize noFeignClient */
 @Component
-public class MinIOFeignClient {
+public class MinIONoFeignClient {
 
     @Bean
     @LoadBalanced
