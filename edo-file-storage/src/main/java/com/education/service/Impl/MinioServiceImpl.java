07ea3873f6f3723c0a817678b00a3777776a941b
Akhmed Guseynov <akhmeddzhan@mail.ru>
2023-01-08 13:39:29 +0000

    Update 2 files
    - /edo-file-storage/src/main/java/com/education/service/Impl/MinioServiceImpl.java
    - /edo-scheduler/src/main/java/com/education/minio/SchedulerForMinioService.java

--- edo-file-storage/src/main/java/com/education/service/Impl/MinioServiceImpl.java
+++ edo-file-storage/src/main/java/com/education/service/Impl/MinioServiceImpl.java
@@ -31,9 +31,6 @@ public class MinioServiceImpl implements MinioService {
                 minioClient.makeBucket(MakeBucketArgs.builder().bucket("my-bucketname").build());
                 System.out.println("my-bucketname is created successfully");
             }
-//            // Enable versioning on 'my-bucketname'
-//            minioClient.setBucketVersioning(SetBucketVersioningArgs.builder().bucket("my-bucketname").build());
-//            System.out.println("Bucket versioning is enabled successfully");
         } catch (MinioException e) {
             System.out.println("checking for bucket`s existence : " + e.getMessage());
         } catch (Exception e) {
@@ -45,7 +42,7 @@ public class MinioServiceImpl implements MinioService {
     {
         Map<String, ZonedDateTime> lastmdf = new HashMap<>();
         try{
-            // Lists objects information.
+            //** Lists objects information. */ 
             Iterable<Result<Item>> results =
                     minioClient.listObjects(ListObjectsArgs.builder().bucket("my-bucketname").build());
             for (Result<Item> result : results) {
@@ -74,11 +71,8 @@ public class MinioServiceImpl implements MinioService {
     public void checkConnection() {
         try {
             List<Bucket> blist = minioClient.listBuckets();
-            System.out.println(minioClient.bucketExists(BucketExistsArgs.builder().bucket("try1").build()));
-            System.out.println(minioClient.getObject(GetObjectArgs.builder().bucket("try1").object("text1.txt").build()));
-            System.out.println();
+            System.out.println(minioClient.bucketExists(BucketExistsArgs.builder().bucket("my-bucketname").build()));
             System.out.println("Connection success, total buckets: " + blist.size());
-            System.out.println(minioClient.getBucketTags(GetBucketTagsArgs.builder().bucket("try1").build()));
         } catch (MinioException e) {
             System.out.println("Connectionfailed for AKhmed: " + e.getMessage());
         } catch (Exception e) {
@@ -86,6 +80,7 @@ public class MinioServiceImpl implements MinioService {
         }
     }
 
+    //** Upload 'my-objectname' from 'targetfolder' to 'sourcefolder' */ 
     @Override
     public void uploadOneFile(String objectName) {
         try{
@@ -103,9 +98,10 @@ public class MinioServiceImpl implements MinioService {
         }
     }
 
+    //** Download 'my-objectname' from 'my-bucketname' to 'targetfolder' */
     @Override
     public void downloadOneFile(String objectName) {
-        // Download 'my-objectname' from 'my-bucketname' to 'my-filename'
+         
         try{
             minioClient.downloadObject(
                     DownloadObjectArgs.builder()
