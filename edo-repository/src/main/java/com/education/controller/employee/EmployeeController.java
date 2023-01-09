ce28a4a329e05765d1b5abad7cd5d7926e3b1e2f
KiladzeGM <kiladzegm@gmail.com>
2023-01-10 01:26:23 +0500

    Fix Employee environment

---
+++ edo-repository/src/main/java/com/education/controller/employee/EmployeeController.java
@@ -0,0 +1,181 @@
+package com.education.controller.employee;
+
+import com.education.entity.Employee;
+import com.education.service.employee.impl.EmployeeServiceImpl;
+import io.swagger.annotations.ApiOperation;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import model.dto.EmployeeDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author George Kiladze
+ * Rest-контроллер отправляет запрос от клиента в бд
+ */
+@Log4j2
+@ApiOperation(value = "Контроллер сотрудника")
+@RestController
+@RequestMapping("/api/repository/employee")
+@AllArgsConstructor
+public class EmployeeController {
+
+    private final EmployeeServiceImpl employeeService;
+
+    /**
+     * предоставляет сотрудника по id
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "Предоставление сотрудника по индентификатору")
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EmployeeDto> findById(@PathVariable Long id) {
+        log.info("Send a response with the employee of the assigned id");
+        EmployeeDto employeeDto = toDto(employeeService.findById(id));
+        log.info("The operation was successful, we got the employee by id ={}", id);
+        return new ResponseEntity<EmployeeDto>(employeeDto, HttpStatus.OK);
+    }
+
+    /**
+     * предоставляет всех сотрудников
+     *
+     * @return
+     */
+    @ApiOperation(value = "Предоставление сотрудников по назначеным идентификаторам")
+    @GetMapping("/all")
+    public ResponseEntity<Collection<EmployeeDto>> findAll() {
+        log.info("Send a response with the employees");
+        Collection<EmployeeDto> employeeDto = toDto(employeeService.findAll());
+        log.info("The operation was successful, we got the all employees");
+        return new ResponseEntity<>(employeeDto, HttpStatus.OK);
+    }
+
+    /**
+     * предоставляет сотрудников по некоскольким id
+     *
+     * @param ids
+     * @return
+     */
+    @ApiOperation(value = "Предоставление сотрудников по назначеным идентификаторам")
+    @GetMapping("/all/{ids}")
+    public ResponseEntity<Collection<EmployeeDto>> findAllById(@PathVariable List<Long> ids) {
+        log.info("Send a response with the employee of the assigned IDs");
+        Collection<EmployeeDto> employeeDto = toDto(employeeService.findAllById(ids));
+        log.info("The operation was successful, we got the employee by id = {} ", ids);
+        return new ResponseEntity<>(employeeDto, HttpStatus.OK);
+    }
+
+    /**
+     * добавляет сотрудника
+     *
+     * @param employeeDto
+     * @return
+     */
+    @ApiOperation(value = "Создает сотрудника")
+    @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<EmployeeDto> save(@RequestBody EmployeeDto employeeDto) {
+        log.info("Starting the save operation");
+        employeeService.save(toEntity(employeeDto));
+        log.info("Saving the employee");
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    /**
+     * архивация сотрудника с занесением времени архивации
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "Архивация сотрудника с занесением времени архивации")
+    @PostMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> moveToArchived(@PathVariable Long id) {
+        log.info("Starting the archiving operation");
+        employeeService.moveToArchived(id);
+        log.info("Archiving the employee");
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * предоставляет заархивированного сторудника по id
+     *
+     * @param id
+     * @return
+     */
+    @ApiOperation(value = "Предоставление сотрудника без архивирования по идентификатору")
+    @GetMapping(value = "/NotArchived/{id}")
+    public ResponseEntity<EmployeeDto> findByIdNotArchived(@PathVariable Long id) {
+        log.info("Send a response with the employee not archived of the assigned ID");
+        EmployeeDto employeeDto = toDto(employeeService.findByIdAndArchivedDateNull(id));
+        log.info("The operation was successful, they got the non-archived employee by id ={}", id);
+        return new ResponseEntity<EmployeeDto>(employeeDto, HttpStatus.OK);
+    }
+
+    /**
+     * предоставляет заархивированных сторудников по нескольким id
+     *
+     * @param ids
+     * @return
+     */
+    @ApiOperation(value = "Предоставление сотрудников без архивирования по идентификатору")
+    @GetMapping(value = "/NotArchivedAll/{ids}")
+    public ResponseEntity<Collection<EmployeeDto>> findByAllIdNotArchived(@PathVariable List<Long> ids) {
+        log.info("Send a response with the employee not archived of the assigned IDs");
+        Collection<EmployeeDto> employeeDto = employeeService.findByIdInAndArchivedDateNull(ids).stream().map(this::toDto).toList();
+        log.info("The operation was successful, they got the non-archived employee by id ={}", ids);
+        return new ResponseEntity<Collection<EmployeeDto>>(employeeDto, HttpStatus.OK);
+    }
+
+    public EmployeeDto toDto(Employee employee) {
+
+        return EmployeeDto.builder()
+                .id(employee.getId())
+                .firstName(employee.getFirstName())
+                .lastName(employee.getLastName())
+                .middleName(employee.getMiddleName())
+                .address(employee.getAddress())
+                .fioDative(employee.getFioDative())
+                .fioNominative(employee.getFioNominative())
+                .fioGenitive(employee.getFioGenitive())
+                .externalId(employee.getExternalId())
+                .phone(employee.getPhone())
+                .workPhone(employee.getWorkPhone())
+                .birthDate(employee.getBirthDate())
+                .username(employee.getUsername())
+                .creationDate(employee.getCreationDate())
+                .archivedDate(employee.getArchivedDate())
+                .build();
+
+    }
+
+    public List<EmployeeDto> toDto(Collection<Employee> employee) {
+        return employee.stream().map(this::toDto).toList();
+    }
+
+    public Employee toEntity(EmployeeDto employeeDto) {
+
+        return Employee.employeeBuilder()
+                .firstName(employeeDto.getFirstName())
+                .lastName(employeeDto.getLastName())
+                .middleName(employeeDto.getMiddleName())
+                .address(employeeDto.getAddress())
+                .fioDative(employeeDto.getFioDative())
+                .fioNominative(employeeDto.getFioNominative())
+                .fioGenitive(employeeDto.getFioGenitive())
+                .externalId(employeeDto.getExternalId())
+                .phone(employeeDto.getPhone())
+                .workPhone(employeeDto.getWorkPhone())
+                .birthDate(employeeDto.getBirthDate())
+                .username(employeeDto.getUsername())
+                .creationDate(employeeDto.getCreationDate())
+                .archivedDate(employeeDto.getArchivedDate())
+                .build();
+
+    }
+
+}
---
+++ edo-service/src/main/java/com/education/service/emloyee/impl/EmployeeServiceImpl.java
@@ -0,0 +1,68 @@
+package com.education.service.emloyee.impl;
+
+import com.education.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import model.dto.EmployeeDto;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+
+
+/**
+ * @author Kiladze George
+ * Сервис, который соединяет реализацию двух модулей через RestTemplate
+ * Имеет все те же операции, что и service в edo-repository
+ */
+@Service
+@Log4j2
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService {
+
+    static final String URL = "http://edo-repository/api/repository/employee";
+    private final RestTemplate restTemplate;
+
+    @Override
+    public void save(EmployeeDto employeeDto) {
+        restTemplate.postForObject(URL, employeeDto, EmployeeDto.class);
+        log.info("Sent a request to save the employee in edo - repository");
+    }
+
+    @Override
+    public void moveToArchived(Long id) {
+        restTemplate.postForObject(URL + "/" + id, null, String.class);
+        log.info("Sent a request to archive the employee in edo - repository");
+    }
+
+    @Override
+    public EmployeeDto findById(Long id) {
+        log.info("Sent a request to receive the employee in edo - repository");
+        return restTemplate.getForObject(URL + "/" + id, EmployeeDto.class);
+    }
+
+    @Override
+    public Collection<EmployeeDto> findAll() {
+        log.info("Sent a request to receive all employee in edo - repository");
+        return restTemplate.getForObject(URL + "/all", List.class);
+    }
+
+    @Override
+    public Collection<EmployeeDto> findAllById(String ids) {
+        log.info("Sent a request to receive the employee in edo - repository");
+        return restTemplate.getForObject(URL + "/all/" + ids, List.class);
+    }
+
+    @Override
+    public EmployeeDto findByIdAndArchivedDateNull(Long id) {
+        log.info("Sent a request to receive the employee archived in edo - repository");
+        return restTemplate.getForObject(URL + "/NotArchived/" + id, EmployeeDto.class);
+    }
+
+    @Override
+    public Collection<EmployeeDto> findByIdInAndArchivedDateNull(String ids) {
+        log.info("Sent a request to receive the employee not archived in edo - repository");
+        return restTemplate.getForObject(URL + "/NotArchivedAll/" + ids, List.class);
+    }
+}
