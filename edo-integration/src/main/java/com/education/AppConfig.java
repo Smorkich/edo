a4a6c5c0f0789c5ec3813ff5dd2184da702b4bb2
Nadezhda Pupina <goncharova.hope@gmail.com>
2023-01-13 14:07:17 +0300

    6_Sending_Email_to_Author

--- edo-integration/src/main/java/com/education/AppConfig.java
+++ edo-integration/src/main/java/com/education/AppConfig.java
@@ -1,36 +1,7 @@
 package com.education;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.mail.MailSender;
-import org.springframework.mail.javamail.JavaMailSenderImpl;
-
-import java.util.Properties;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public MailSender mailSender() {
-
-        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-
-        mailSender.setHost("smtp.gmail.com");
-        mailSender.setPort(587);
-        mailSender.setUsername("noreply.ros.edo@gmail.com");
-        mailSender.setPassword("1111111111");
-
-        Properties properties = new Properties();
-
-        properties.put("mail.transport.protocol", "smtp");
-        properties.put("mail.smtp.auth", "true");
-        properties.put("mail.smtp.starttls.enable", "true");
-        properties.put("mail.debug", "true");
-
-        mailSender.setJavaMailProperties(properties);
-
-        return mailSender;
-
-    }
-
 }
--- edo-integration/src/main/java/com/education/EdoIntegrationApplication.java
+++ edo-integration/src/main/java/com/education/EdoIntegrationApplication.java
@@ -2,15 +2,11 @@ package com.education;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class EdoIntegrationApplication {
     public static void main(String[] args) {
         SpringApplication.run(EdoIntegrationApplication.class, args);
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
 
-        SendEmail bean = context.getBean(SendEmail.class);
-        bean.sendMail();
     }
 }
