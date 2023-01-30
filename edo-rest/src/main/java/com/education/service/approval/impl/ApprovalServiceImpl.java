f21ff25943561a3c2add5d5d355ab8ac1100ae62
Kuznetsov Kirill <791178429262@mail.ru>
2023-01-30 22:05:14 +0300

    create service in edo-rest

---
+++ edo-rest/src/main/java/com/education/service/approval/impl/ApprovalServiceImpl.java
@@ -0,0 +1,87 @@
+package com.education.service.approval.impl;
+
+import com.education.service.approval.ApprovalService;
+import com.education.util.URIBuilderUtil;
+import lombok.AllArgsConstructor;
+import model.dto.ApprovalDto;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collection;
+import java.util.List;
+
+import static model.constant.Constant.EDO_SERVICE_NAME;
+
+@Service
+@AllArgsConstructor
+public class ApprovalServiceImpl implements ApprovalService {
+    private final RestTemplate restTemplate;
+    @Override
+    public ApprovalDto save(ApprovalDto approvalDto) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval").toString();
+
+        return restTemplate.postForObject(uri, approvalDto, ApprovalDto.class);
+    }
+
+    @Override
+    public ApprovalDto findById(Long id) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/" + id).toString();
+
+        return restTemplate.getForObject(uri, ApprovalDto.class);
+    }
+
+    @Override
+    public Collection<ApprovalDto> findAll() {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/all").toString();
+
+        return restTemplate.getForObject(uri, List.class);
+    }
+
+    @Override
+    public Collection<ApprovalDto> findAllById(Iterable<Long> ids) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/all/" + ids).toString();
+
+        return restTemplate.getForObject(uri, List.class);
+    }
+
+    @Override
+    public void delete(Long id) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/" + id).toString();
+
+        restTemplate.delete(uri);
+    }
+
+    @Override
+    public ApprovalDto moveToArchive(Long id) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/move/" + id).toString();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        return restTemplate.exchange(uri, HttpMethod.PATCH, new HttpEntity<>(headers), ApprovalDto.class).getBody();
+    }
+
+    @Override
+    public ApprovalDto findByIdNotArchived(Long id) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/findByIdNotArchived/" + id).toString();
+
+        return restTemplate.getForObject(uri, ApprovalDto.class);
+    }
+
+    @Override
+    public Collection<ApprovalDto> findAllNotArchived() {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/findAllNotArchived").toString();
+
+        return restTemplate.getForObject(uri, List.class);
+    }
+
+    @Override
+    public Collection<ApprovalDto> findByIdInAndArchivedDateNull(Iterable<Long> ids) {
+        String uri = URIBuilderUtil.buildURI(EDO_SERVICE_NAME, "/api/service/approval/archive/all/findByIdInAndArchivedDateNull/" + ids).toString();
+
+        return restTemplate.getForObject(uri, List.class);
+    }
+}
