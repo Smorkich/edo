1fb501ffb26da0b32ce3ad4d59fcd63cf76e0e6d
svtart <svetoy_a@mail.ru>
2023-01-30 18:34:34 +0300

    Version 1.0 Updated

--- edo-common/src/main/java/model/constant/Constant.java
+++ edo-common/src/main/java/model/constant/Constant.java
@@ -18,4 +18,17 @@ public class Constant {
     public static final String RESOLUTION_URL = "api/repository/resolution";
     public static final String THEME_URL = "api/repository/theme";
 
+    public static final String EDO_REPOSITORY_NAME = "edo-repository";
+    public static final String EDO_SERVICE_NAME = "edo-service";
+    public static final String EMPLOYEE_FIO_SEARCH_PARAMETER = "fullName";
+    public static final String EDO_FILE_STORAGE_NAME = "edo-file-storage";
+
+    // Extensions
+    public static final String PNG = "png";
+    public static final String JPEG = "jpeg";
+    public static final String JPG = "jpg";
+    public static final String DOC = "doc";
+    public static final String DOCX = "docx";
+    public static final String PDF = "pdf";
+
 }
