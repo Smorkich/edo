f704d17ec16afbc56828e8463e75151d75a2c21a
Anton Suhankin <anton.sukhankin@mail.ru>
2023-03-01 14:30:39 +0300

    Исправил загрузку PNG файлов в MINIO

--- edo-file-storage/src/main/java/com/education/component/MinioComponent.java
+++ edo-file-storage/src/main/java/com/education/component/MinioComponent.java
@@ -10,13 +10,12 @@ import io.minio.messages.Bucket;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.pdfbox.cos.COSName;
 import org.apache.pdfbox.filter.MissingImageReaderException;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.PDPageContentStream;
 import org.apache.pdfbox.pdmodel.common.PDRectangle;
-import org.apache.pdfbox.pdmodel.graphics.color.PDDeviceRGB;
+import org.apache.pdfbox.pdmodel.graphics.image.JPEGFactory;
 import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 import org.docx4j.Docx4J;
 import org.docx4j.convert.out.FOSettings;
@@ -163,24 +162,20 @@ public class MinioComponent {
             BufferedImage awtImage = readImageFile(byteStream, extension);
             byteStream.reset();
 
-            PDImageXObject pdImage = new PDImageXObject(doc, byteStream,
-                    COSName.DCT_DECODE, awtImage.getWidth(), awtImage.getHeight(),
-                    awtImage.getColorModel().getComponentSize(0),
-                    PDDeviceRGB.INSTANCE);
+            PDImageXObject pdImage1 = JPEGFactory.createFromImage(doc,awtImage);
 
-            int width = pdImage.getWidth();
-            int height = pdImage.getHeight();
+            int width = pdImage1.getWidth();
+            int height = pdImage1.getHeight();
 
             PDPage myPage = new PDPage(new PDRectangle(width, height));
             doc.addPage(myPage);
 
-            float offset = 20f;
-
-            try (PDPageContentStream cont = new PDPageContentStream(doc, myPage)) {
-                cont.drawImage(pdImage, offset, offset, width, height);
+            try (PDPageContentStream cont = new PDPageContentStream(doc, myPage, PDPageContentStream.AppendMode.APPEND, true, true)) {
+                cont.drawImage(pdImage1, 0, 0);
             }
 
             doc.save(bos);
+            doc.close();
             return new ByteArrayInputStream(bos.toByteArray());
         } catch (IOException e) {
             log.error("Convert image to PDF failed. File name: {}", file.getOriginalFilename());
