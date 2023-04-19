a44a0f046c6174f10b3b5068fae8ce913306edc1
Dmitry Zoteev <nike3.68@mail.ru>
2023-04-19 19:51:11 +0500

    88. Added a constant with the name of the queue

--- edo-common/src/main/java/model/constant/Constant.java
+++ edo-common/src/main/java/model/constant/Constant.java
@@ -25,6 +25,8 @@ public class Constant {
     public static final String EDO_FILE_STORAGE_NAME = "edo-file-storage";
     public static final String NOTIFICATION_URL = "api/repository/notification";
     public static final String REST_TO_SERVICE_APPROVAL_QUEUE = "RestToServiceApprovalQueue";
+
+    public static final String REST_TO_SERVICE_APPEAL_QUEUE = "RestToServiceAppealQueue";
     public static final String FILE_RECOGNITION_START = "FileRecognitionStart";
     public static final String DEADLINE_CHANGE_QUEUE = "DeadlineChange";
 }
