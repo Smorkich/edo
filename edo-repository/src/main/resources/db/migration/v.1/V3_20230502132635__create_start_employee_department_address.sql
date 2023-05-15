a554de28582b7ed348db5cc0fb11c7c9462ad91f
Alexander Borisov <namaste_95@mail.ru>
2023-05-15 19:24:28 +0300

    Closes #96. Created a Dockerfile for edo-scheduler. Fixed script.

--- edo-repository/src/main/resources/db/migration/v.1/V3_20230502132635__create_start_employee_department_address.sql
+++ edo-repository/src/main/resources/db/migration/v.1/V3_20230502132635__create_start_employee_department_address.sql
@@ -1,16 +1,16 @@
-INSERT INTO edo.address (id, full_address, street, house, "index", housing, building, city,
+INSERT INTO address (id, full_address, street, house, "index", housing, building, city,
                          region, country, flat, longitude, latitude)
 VALUES (1, 'test_address', 'test_street', '1', 127031, '3', '1', 'test_city',
         'test_region', 'test_country', 'test_flat', 'test_longitude', 'test_latitude')
 ON CONFLICT (id) DO NOTHING;
 
 
-INSERT INTO edo.department (id, short_name, full_name, address, external_id, phone, creation_date, archived_date)
+INSERT INTO department (id, short_name, full_name, address, external_id, phone, creation_date, archived_date)
 VALUES (1, 'test_short_name', 'test_full_name', 1, '1', 'test_phone', '2023-05-02', '2023-05-02')
 ON CONFLICT (id) DO NOTHING;
 
 
-INSERT INTO edo.employee(id, first_name, last_name, middle_name, fio_dative, fio_nominative,
+INSERT INTO employee(id, first_name, last_name, middle_name, fio_dative, fio_nominative,
                          fio_genitive, external_id, phone, work_phone, birth_date, username,
                          creation_date, archived_date, department, address)
 VALUES (1, 'test_first_name', 'test_last_name','test_middle_name', 'test_fio_dative', 'test_fio_nominative',
