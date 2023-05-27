97caebbdc34309ce4dd634cd543e5707ea413ae9
erosh <erohsenko.roma@bk.ru>
2023-05-27 10:51:57 +0300

    Принимает id обращения и сам файл, после связывает file_pool с appeal"

--- edo-rest/src/main/java/com/education/controller/appeal/AppealController.java
+++ edo-rest/src/main/java/com/education/controller/appeal/AppealController.java
@@ -1,20 +1,19 @@
 package com.education.controller.appeal;
 
 import com.education.service.appeal.AppealService;
+import com.education.service.minio.MinioService;
 import io.swagger.annotations.ApiOperation;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import model.dto.AppealDto;
+import model.dto.FilePoolDto;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Collection;
 
 /**
  * Rest-контроллер в "edo-rest", служит для отправки обращения (Appeal) в БД используя RestTemplate
@@ -25,6 +24,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/edo/appeal")
 public class AppealController {
     private final AppealService appealService;
+
+    private final MinioService minioService;
 
     @ApiOperation(value = "Принимает обращение, отправляет на edo-service", notes = "Обращение должен существовать")
     @PostMapping(value = "", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -49,6 +50,20 @@ public class AppealController {
         log.info("Получаем AppealDto по id");
         var appealDto = appealService.findById(id);
         log.info("AppealDto успешно получен");
+        return new ResponseEntity<>(appealService.findById(id), HttpStatus.OK);
+    }
+    @ApiOperation(value = "Добавляет файл к выбранному обращению ")
+    @PostMapping("/upload")
+    public ResponseEntity<AppealDto> uploadFile(@RequestParam(value = "id", required = true) Long id , @RequestParam(value = "file", required = true) MultipartFile file) {
+        log.info("'AppealController' - Получаем AppealDto по id");
+        AppealDto appealDto = appealService.findById(id);
+
+        log.info("'AppealController' - Загружаем файл в MINIO и получаем FilePoolDto");
+        FilePoolDto filePoolDto = minioService.uploadOneFile(file);
+
+        log.info("'AppealController' - Прикрепляем файл к appeal");
+        appealDto.getFile().add(filePoolDto);
+        appealService.save(appealDto);
         return new ResponseEntity<>(appealService.findById(id), HttpStatus.OK);
     }
 }
