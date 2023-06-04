9aa4707cd8ba8832745e6997e710942a6d1ddb94
Хохрин Дмитрий <d.vasin2016@gmail.com>
2023-06-04 20:28:23 +0300

    generating a number from a template

--- edo-service/src/main/java/com/education/service/appeal/impl/AppealServiceImpl.java
+++ edo-service/src/main/java/com/education/service/appeal/impl/AppealServiceImpl.java
@@ -75,9 +75,14 @@ public class AppealServiceImpl implements AppealService {
 
         try {
             NomenclatureDto nomenclatureDto = appealDto.getNomenclature();
+            nomenclatureDto.getTemplate();
             Calendar calendar = Calendar.getInstance();
             var year = calendar.get(Calendar.YEAR) % 100;
-            appealDto.setNumber(nomenclatureDto.getIndex() + "-" + year + "/2" + "-" + nomenclatureDto.getCurrentValue());
+            var number = nomenclatureDto.getTemplate()
+                    .replace("%ИНДЕКС",nomenclatureDto.getIndex())
+                    .replace("%ГОД",Integer.toString(year))
+                    .replace("%НОМЕР",Long.toString(nomenclatureDto.getCurrentValue()));
+            appealDto.setNumber(number);
 
         } catch (Exception e) {
             e.printStackTrace();
