aff74eb92de2a921ef4d0f07744989b8c0d8d14d
Vasiljev Kirill <cool.kirill.vasiljev.01@yandex.ru>
2023-07-24 19:09:01 +0300

    Changing RabbitMQ and move Config class to edo-common module. Implemented queues autoloading when starting any modules

--- edo-common/src/main/java/config/RabbitConfig.java
+++ edo-common/src/main/java/config/RabbitConfig.java
@@ -1,23 +1,24 @@
-package com.education.config;
+package config;
 
 import org.springframework.amqp.core.AmqpAdmin;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
-import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import static model.constant.Constant.REST_TO_SERVICE_APPROVAL_QUEUE;
+import static model.constant.Constant.*;
 
 @EnableRabbit
 @Configuration
 public class RabbitConfig {
+
 
     @Value("${spring.rabbitmq.host}")
     private String host;
@@ -25,7 +26,7 @@ public class RabbitConfig {
     @Value("${spring.rabbitmq.port}")
     private Integer port;
 
-    @Bean
+    @Bean(name = "SimpleRabbitListener")
     public SimpleRabbitListenerContainerFactory
     rabbitListenerContainerFactory(ConnectionFactory connectionFactory) {
         SimpleRabbitListenerContainerFactory factory = new
@@ -35,23 +36,53 @@ public class RabbitConfig {
         return factory;
     }
 
-    @Bean
+    @Bean(name = "connection")
     public ConnectionFactory connectionFactory() {
         return new CachingConnectionFactory(host, port);
     }
 
-    @Bean
+    @Bean(name = "amqpAdmin")
     public AmqpAdmin amqpAdmin() {
         return new RabbitAdmin(connectionFactory());
     }
 
-    @Bean
+    @Bean(name = "rabbitTemplate")
     public RabbitTemplate rabbitTemplate() {
         return new RabbitTemplate(connectionFactory());
     }
 
-    @Bean
-    public Queue queue() {
-        return new Queue(REST_TO_SERVICE_APPROVAL_QUEUE);
+    @Bean(name = "nomenclature")
+    public Queue nomenclatureQueue() {
+        Queue queue = new Queue(REST_TO_SERVICE_NOMENCLATURE_QUEUE);
+        amqpAdmin().declareQueue(queue);
+        return queue;
+    }
+
+    @Bean(name = "approval")
+    public Queue approvalQueue() {
+        Queue queue = new Queue(REST_TO_SERVICE_APPROVAL_QUEUE);
+        amqpAdmin().declareQueue(queue);
+        return queue;
+    }
+
+    @Bean(name = "appeal")
+    public Queue appealQueue() {
+        Queue queue = new Queue(REST_TO_SERVICE_APPEAL_QUEUE);
+        amqpAdmin().declareQueue(queue);
+        return queue;
+    }
+
+    @Bean(name = "fileRecognition")
+    public Queue fileRecognitionQueue() {
+        Queue queue = new Queue(FILE_RECOGNITION_START);
+        amqpAdmin().declareQueue(queue);
+        return queue;
+    }
+
+    @Bean(name = "deadlineChange")
+    public Queue deadlineChangeQueue() {
+        Queue queue = new Queue(DEADLINE_CHANGE_QUEUE);
+        amqpAdmin().declareQueue(queue);
+        return queue;
     }
 }
