6125cdc62c0c55cb2e067914a327114e48175d8a
Anders-o-n <jvsout@gmail.com>
2023-08-11 17:05:37 +0700

    Revert "Александр Корякин (задача 130 - Перевести запросы Theme на FeignClient)"
    This reverts commit a1db9fdca23010ab5e82b7900a3762ba7b6b0332.

--- edo-service/src/main/java/com/education/service/theme/imp/ThemeServiceImp.java
+++ edo-service/src/main/java/com/education/service/theme/imp/ThemeServiceImp.java
@@ -24,62 +24,32 @@ public class ThemeServiceImp implements ThemeService {
 
     @Override
     public void save(ThemeDto themeDto) {
-        //log.info("sent a request to save the theme in edo - repository");
-        //var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL);
-
-        //restTemplate.postForObject(builder.toString(), themeDto, ThemeDto.class);
-        //log.info("sent a request to save the theme in edo - repository");
         themeFeignClient.save(themeDto);
     }
 
     @Override
     public void moveToArchived(Long id) {
-        // var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL)
-        //       .setPath("/")
-        //       .setPath(String.valueOf(id));
-        //restTemplate.postForObject(builder.toString(), null, ThemeDto.class);
-        // log.info("sent a request to archive the theme in edo - repository");
         themeFeignClient.moveToArchive(id);
     }
 
     @Override
     public ThemeDto findById(Long id) {
-//      log.info("sent a request to receive the theme in edo - repository");
-//        var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL)
-//                .setPath("/")
-//                .setPath(String.valueOf(id));
-//        return restTemplate.getForObject(builder.toString(), ThemeDto.class);
         return themeFeignClient.findById(id);
     }
 
     @Override
     public Collection<ThemeDto> findByAllId(String ids) {
-//        log.info("sent a request to receive the theme in edo - repository");
-//        var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL)
-//                .setPath("/all/")
-//                .setPath(String.valueOf(ids));
-//        return restTemplate.getForObject(builder.toString(), List.class);
         return themeFeignClient.findByAllId(ids);
 
     }
 
     @Override
     public ThemeDto findByIdNotArchived(Long id) {
-//        log.info("sent a request to receive the theme not archived in edo - repository");
-//        var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL)
-//                .setPath("/NotArchived/")
-//                .setPath(String.valueOf(id));
-//        return restTemplate.getForObject(builder.toString(), ThemeDto.class);
         return themeFeignClient.findByIdNotArchived(id);
     }
 
     @Override
     public Collection<ThemeDto> findByAllIdNotArchived(String ids) {
-//        log.info("sent a request to receive the theme not archived in edo - repository");
-//        var builder = buildURI(EDO_REPOSITORY_NAME, THEME_URL)
-//                .setPath("/NotArchivedAll/")
-//                .setPath(String.valueOf(ids));
-//        return restTemplate.getForObject(builder.toString(), List.class);
         return  themeFeignClient.findByAllIdNotArchived(ids);
     }
 }
